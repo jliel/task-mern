32fdd88b0d622a40733f4218213cee1062846817
jliel <kiritobaz@gmail.com>
2024-03-31 08:57:23 -0600

    changes in login, logout broken

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { FaSignInAlt } from "react-icons/fa"
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from './Spinner'
 
 const Login = () => {
     const [formData, setFormData] = useState({ email: '', password: '' })
     const { email, password } = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth)
 
     const onChange = e => {
         setFormData(prevState => ({
@@ -12,10 +22,19 @@ const Login = () => {
         }))
     }
 
+    useEffect(() => {
+        if(isError) toast.error(message)
+        if(isSuccess || user) navigate('/')
+        dispatch(reset())
+    }, [user, isError, isSuccess, message, navigate, dispatch])
+
     const onSubmit = e => {
         e.preventDefault()
+        const userData = {email, password}
+        dispatch(login(userData))
     }
     return (
+        isLoading ? <Spinner/> :
         <>
             <section className="heading">
                 <h1><FaSignInAlt />Login</h1>
